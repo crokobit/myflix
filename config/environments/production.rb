ce71c5428f37735dbcfca9a9ac94ea1b6f4bbf1d
crokobit <crokobit@gmail.com>
2014-06-07 05:01:31 +0800

    production gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,17 @@
 Myflix::Application.configure do
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: "crokobit.web.dev@gmail.com",
+    password: "rails1201",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost:3000'}
 
   config.cache_classes = true
