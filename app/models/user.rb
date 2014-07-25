6201d860e17bfbe9eb5acf2425d7db0e4e465c6b
crokobit <crokobit@gmail.com>
2014-07-25 21:35:42 +0800

    add email uniqueness validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :password, presence: true
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   
   has_many :reviews, -> { order(created_at: :desc) }
   has_many :queue_items
