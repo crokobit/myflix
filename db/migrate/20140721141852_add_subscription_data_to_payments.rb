c0726281cc9ffbcbbf28dd06a4c97f8c01cb399a
crokobit <crokobit@gmail.com>
2014-07-30 12:46:40 +0800

    modify payment data type

--- db/migrate/20140721141852_add_subscription_data_to_payments.rb
+++ db/migrate/20140721141852_add_subscription_data_to_payments.rb
@@ -1,7 +1,7 @@
 class AddSubscriptionDataToPayments < ActiveRecord::Migration
   def change
-    add_column :payments, :start_time, :time
-    add_column :payments, :end_time, :time
+    add_column :payments, :start_time, :datetime
+    add_column :payments, :end_time, :datetime
     add_column :payments, :cancel_at_period_end, :boolean
   end
 end
